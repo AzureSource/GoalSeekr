0b060a7ebe47ed713d985475d65376fba5a6b4ef
Marshall Zmuda <marshall.zmuda@gmail.com>
2022-08-12 17:03:04 -0700

    pre merge commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { Button, Textarea } from '@chakra-ui/react';
 import React from 'react';
+import ShipModal from './UI/modal.js';
+
+
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <div>
+      App
+    </div>
   );
 };
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 document.documentElement.setAttribute('lang', 'en');
 const container = document.createElement('div');
 container.setAttribute('id', 'root');
 document.body.appendChild(container);
 const root = createRoot(container);
-root.render(<App />);
+
+
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
