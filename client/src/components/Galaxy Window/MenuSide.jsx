3401c15656945a3cc4dbc437595c567b6db29cfd
Marshall Zmuda <marshall.z@outlook.com>
2022-08-13 20:08:08 -0700

    adjust side menu css

--- client/src/components/Galaxy Window/MenuSide.jsx
+++ client/src/components/Galaxy Window/MenuSide.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Flex, Spacer } from '@chakra-ui/react';
-import Playerlist from '../list/PlayerList.jsx';
+import Playerlist  from '../list/PlayerList.jsx';
 
 const MenuSide = () => {
   return (
-    <Flex className='menu-side'
+    <Flex
+      className='menu-side-container'
       justify='center'
+      alignItems='center'
+      margin='2rem'
+      width='20%'
+      display='block'
     >
-      <Flex
-        className='menu-side-container'
-        justify='center'
-        alignItems='center'
-      >
-        <Playerlist />
-        all side menu components
-      </Flex>
-
+      <Playerlist />
+      all side menu components
     </Flex>
+
   );
 };
 
