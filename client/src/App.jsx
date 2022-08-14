e7a08af0e4dbfb19fe91973b4574400ce2bb8ef5
Jade Zhang <yxzhang1009@gmail.com>
2022-08-13 18:15:29 -0700

    google authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { signInWithGoogle } from './firebase.js';
 import Counter from './components/actionsToolbar/attackModal/Counter';
+import '../assets/login.css';
 
 const App = () => {
   return (
     <div>
       <div className='app'>
-        test
+        <div>
+          <button className="login-with-google-btn" onClick = {signInWithGoogle}> Sign In With Google</button>
+        </div>
       </div>
       <Counter />
     </div>
