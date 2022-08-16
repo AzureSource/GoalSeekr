89d0b1b317e8dd16335ea868d0bd4b20503782f8
Jade Zhang <yxzhang1009@gmail.com>
2022-08-15 23:30:25 -0700

    delete duplicate file

--- server/models/login.js
+++ server/models/login.js
@@ -3,21 +3,24 @@ const client = require('../../database');
 module.exports = {
   //post
   checkUser: function (req, res) {
+    // const query = `SELECT createorupdateuser(${req.body.googleuid},${req.body.displayname},${req.body.email})`;
+    // client(query)
     const query = 'SELECT createorupdateuser($1, $2, $3, $4, $5, $6)';
     client(query, [req.body.googleuid, req.body.displayname, req.body.email, 'motto', 'about', 'url'])
       .then((result) =>
+      //console.log(result.rows);
         res.json(result.rows[0].createorupdateuser.id)
       )
       .catch(() => res.status(500).send('Error in checking existing user using google uid'));
   },
   //put
   checkGalaxyName: function (req, res) {
-   // console.log(req.query);
+    // console.log(req.query);
     client('SELECT id FROM galaxies WHERE name = $1',[req.query.name])
-    .then(({rows}) =>{
-      console.log(rows);
-      res.status(200).json(rows)
-    })
+      .then(({rows}) =>{
+        console.log(rows);
+        res.status(200).json(rows);
+      })
       .catch(() => res.status(400).send('Error in check galaxy name'));
   }
 };
