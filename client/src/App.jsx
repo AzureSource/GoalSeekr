9562404a0408c07714a82d683d967031ae15dba6
Isabelle <izzigrace6@gmail.com>
2022-08-18 12:31:10 -0700

    some more css and ship pictures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,51 +8,49 @@ import GalaxyWindow from './components/Galaxy Window/GalaxyWindow.jsx';'react-zo
 import LoginAuth from './components/Login/LoginAuth.jsx';
 import TaskTracker from './components/TaskTracker/TaskTracker.jsx';
 
-const App = () => {
+// const App = () => {
 
-  const [title, setTitle] = useState(true);
-  return (
-    <div className='appBackground'>
-      <div className='app'>
-        <GalaxyWindow setTitle={setTitle} className='galaxyWindow' />
-      </div>
-    </div>
-  );
-};
-
-// export default App;
-//     <HashRouter>
-//       <div className='appBackground'>
-//         {title ? <TitleBar/> : null}
-//         <Routes>
-//           <Route
-//             exact path="/"
-//             element={<LoginAuth setTitle={setTitle}/>}
-//           />
-//           <Route
-//             exact path="/entergalaxy/userid/:id"
-//             element = {<EnterGalaxy setTitle={setTitle}/>}
-//           />
-//           <Route
-//             exact path="/creategalaxy/userid/:id"
-//             element={<CreateGalaxy setTitle={setTitle}/>}
-//           />
-//           <Route
-//             exact path = "/galaxy/userid/:id"
-//             element={<GalaxyWindow setTitle={setTitle}/>}
-//           />
-//           <Route
-//             exact path = "/tasks/userid/:id"
-//             element={<TaskTracker setTitle={setTitle}/>}
-//           />
-//         </Routes>
+//   const [title, setTitle] = useState(true);
+//   return (
+//     <div className='appBackground'>
+//       <div className='app'>
+//         <GalaxyWindow setTitle={setTitle} className='galaxyWindow' />
 //       </div>
-//       {/*<button className="login-with-google-btn" onClick = {signInWithGoogle}> Sign In With Google</button>*/}
-//       {/* <CreateGalaxy /> */}
-//       {/* <Title /> */}
-//       {/* <Menu /> */}
-//     </HashRouter>
+//     </div>
 //   );
 // };
+
+const App = () => {
+  const [title, setTitle] = useState(true);
+  return (
+    <HashRouter>
+      <div className='appBackground'>
+        {title ? <TitleBar/> : null}
+        <Routes>
+          <Route
+            exact path="/"
+            element={<LoginAuth setTitle={setTitle}/>}
+          />
+          <Route
+            exact path="/entergalaxy/userid/:id"
+            element = {<EnterGalaxy setTitle={setTitle}/>}
+          />
+          <Route
+            exact path="/creategalaxy/userid/:id"
+            element={<CreateGalaxy setTitle={setTitle}/>}
+          />
+          <Route
+            exact path = "/galaxy/userid/:id"
+            element={<GalaxyWindow setTitle={setTitle}/>}
+          />
+          <Route
+            exact path = "/tasks/userid/:id"
+            element={<TaskTracker setTitle={setTitle}/>}
+          />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+};
 
 export default App;
