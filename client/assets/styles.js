aaf72d5be69ba9343a8ec419dbb44c20e1315d8c
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-08-19 12:15:58 -0700

    implementing update users motto backend and frontend

--- client/assets/styles.js
+++ client/assets/styles.js
@@ -494,12 +494,15 @@ const overrides = {
       '.stats-container': {
         marginRight: '7.5px',
         alignItems: 'center',
+        fontFamily: 'Abril Fatface',
+        color:  'rgba(80,182,171)'
       },
       '.chatsMainDisplay::-webkit-scrollbar': {
         display: 'none'  /* Safari and Chrome */
       },
-      '.chatsSendBox, .chatsMainDisplay, .stats-container, .chatsTitle': {
+      '.chatsSendBox, .chatsMainDisplay, .chatsTitle': {
         fontFamily: 'Abril Fatface',
+        color:  'rgba(80,182,171)'
       },
       //--------------------------------- stats
       //---------------------------------SIDE MENU
@@ -654,7 +657,7 @@ const overrides = {
         justifyContent: 'center',
         width: '85px'
       },
-      '.username-input, .enter-existing-galaxy-input': {
+      '.username-input, .enter-existing-galaxy-input, .motto-input': {
         width: '200px',
         fontFamily: 'Abril Fatface',
         size: '20px',
@@ -662,7 +665,12 @@ const overrides = {
         borderRadius: '4px',
         border:'2px solid #2e2f47',
         height: '37px',
-        margin: '15px 0px'
+      },
+      '.username-input': {
+        marginBottom: '5px',
+      },
+      '.motto-input': {
+        margin: '5px 0px 10px',
       },
       '.enter-existing-galaxy-input': {
         margin: '10px 0px'
--- client/src/components/Login/EnterUserName.jsx
+++ client/src/components/Login/EnterUserName.jsx
@@ -1,15 +1,15 @@
 import React,{useState} from 'react';
 import axios from 'axios';
-import { Button } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
 // eslint-disable-next-line react/prop-types
 export default function EnterUserName ({authData}) {
   const [text,setText] = useState('');
+  const [motto, setMotto] = useState('');
 
   const redirectToEnterGalaxyPage = function(id) {
     window.location.href = `http://localhost:7777/#/entergalaxy/userid/${id}`;
   };
-
   const handleJoin = function(){
     if(text){
       axios({
@@ -26,9 +26,16 @@ export default function EnterUserName ({authData}) {
             axios({
               url:'/api/users',
               method:'post',
-              data:{...authData,displayname:text}
+              data:{
+                ...authData,
+                displayname: text,
+                motto: motto,
+              }
             })
-              .then((result)=>redirectToEnterGalaxyPage(result.data))
+              .then((result)=>{
+
+                redirectToEnterGalaxyPage(result.data)
+              })
               .catch(()=>console.log('Err from enter user name'));
           }
         })
@@ -41,11 +48,18 @@ export default function EnterUserName ({authData}) {
 
   return (
     <div className='username-btn-container'>
-      <input
-        className='username-input'
-        placeholder = "Enter Username Display" onChange ={(event)=>setText(event.target.value)}
-      />
-      <br />
+      <Flex
+        flexDir='column'
+      >
+        <input
+          className='username-input'
+          placeholder = "Enter Username Display" onChange ={(event)=>setText(event.target.value)}
+        />
+        <input
+          className='motto-input'
+          placeholder = "Enter Your Motto" onChange ={(event)=>setMotto(event.target.value)}
+        />
+      </Flex>
       <button
         className='username-input-btn'
         onClick={handleJoin}
