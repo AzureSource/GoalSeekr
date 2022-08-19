c2891c3fb911aa45e6e2ec8419bde860df5e3363
Marshall Zmuda <marshall.z@outlook.com>
2022-08-19 11:40:31 -0700

    active user and next turn state/db

--- server/models/galaxy.js
+++ server/models/galaxy.js
@@ -9,16 +9,16 @@ module.exports = {
     try {
       const u_id = req.params.user_id;
       const g_id = req.params.galaxy_id;
-      
-      const query = `UPDATE galaxies SET opengame = false, activeuser = $1 WHERE id = $2;`
+
+      const query = `UPDATE galaxies SET gamestarted = true, activeuser = $1 WHERE id = $2;`;
       const result = await client(query, [u_id, g_id]);
       res.json(u_id);
-      
+
     } catch (error) {
       res.end().status(500);
     }
-    
   },
+  //takes user id on end turn click and returns next active user
   changeTurn: async function (req, res) {
     try {
       const u_id = req.params.user_id;
@@ -26,16 +26,20 @@ module.exports = {
 
       const query1 = `SELECT id FROM users WHERE currentgalaxy = $1;`;
       const userArray = await client(query1, [g_id]);
-      const nextIndex = userArray.indexOf(u_id) + 1;
+      console.log(userArray);
+      var nextIndex = userArray.rows.map((user) => (user.id.id))
+        .indexOf(u_id) + 1;
+      console.log(nextIndex);
       if (nextIndex > userArray.length - 1) {
         nextIndex = 0;
       }
       const nextUserID = userArray[nextIndex];
 
-      const query2 = `UPDATE galaxies SET activeuser = $1 WHERE id = $2;`
+      const query2 = `UPDATE galaxies SET activeuser = $1 WHERE id = $2;`;
       const result = await client(query2, [nextUserID, g_id]);
-      res.json(result);
-      
+      console.log('change turn result: ', result);
+      res.json(nextUserID);
+
     } catch {
       res.end().status(500);
     }
